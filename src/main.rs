8339f0e83f11dabc3667174bf041c9b3cb90335d
Kevin Jackson <kevin.jackson@thehutgroup.com>
2019-05-09 06:12:10 +0100

    more tidyup and refactoring

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ struct GithubCommitter {
 #[derive(Debug, Deserialize)]
 struct GithubCommitInfo {
     author: GithubCommitter,
-    committer: GithubCommitter,
     message: String
 }
 
@@ -30,40 +29,32 @@ struct GithubCommit {
     commit: GithubCommitInfo,
 }
 
-fn commit_to_formatted_output(commit: Commit, github_repo: &str) -> Result<String, Error> {
+fn commit_to_formatted_output(commit: Commit, github_repo: &str, access_token: &str) -> Result<String, Error> {
     let sha = commit.id().to_string();
     let commit_ts = commit.time().seconds();
     let message = commit.summary().unwrap();
+    let author = commit.author();
 
     match extract_pr_from_commit_message(message) {
         Some(pr_number) => {
-            let branch_time = fetch_github_info_for_commit(commit_ts, pr_number, github_repo).unwrap();
-            match branch_time {
-                Some(bt) => Ok(format!("{},{},{},{}", sha, commit_ts, bt, message).to_owned()),
-                None => Ok(format!("{},{},unknown,{}", sha, commit_ts, message).to_owned())
+            match fetch_github_info_for_commit(commit_ts, pr_number, github_repo, access_token) {
+                Some(bt) => Ok(format!("{},{},{},{},{},{}", sha, commit_ts, pr_number, bt, author.email().unwrap(), message).to_owned()),
+                None => Ok(format!("{},{},unknown,unknown,{},{}", sha, commit_ts, author.email().unwrap(), message).to_owned())
             }
         },
-        None => Ok(format!("{},{},unknown,{}", sha, commit_ts, message).to_owned())
+        None => Ok(format!("{},{},unknown,unknown,{},{}", sha, commit_ts, author.email().unwrap(), message).to_owned())
     }
 }
 
-fn fetch_github_info_for_commit(commit_ts: i64, pr_number: &str, github_repo: &str) -> Result<Option<i64>, Box<dyn std::error::Error>> {
-    match env::var("GITHUB_STATS_TOKEN") {
-        Ok(val) => {
-            let url = format!("https://api.github.com/repos/{}/pulls/{}/commits?access_token={}", github_repo, pr_number, val);
-            let json = reqwest::get(url.as_str())?.json::<Vec<GithubCommit>>()?;
-            match json.get(0) {
-                Some(c) => {
-                    let dt = &c.commit.author.date.parse::<DateTime<Utc>>()?;
-                    Ok(Some(commit_ts - dt.timestamp()))
-                },
-                None => Ok(None)
-            }
+fn fetch_github_info_for_commit(commit_ts: i64, pr_number: &str, github_repo: &str, access_token: &str) -> Option<i64> {
+    let url = format!("https://api.github.com/repos/{}/pulls/{}/commits?access_token={}", github_repo, pr_number, access_token);
+    let json = reqwest::get(url.as_str()).expect("cannot fetch data for commit").json::<Vec<GithubCommit>>().expect("cannot parse data for commit");
+    match json.first() {
+        Some(c) => {
+            let dt = &c.commit.author.date.parse::<DateTime<Utc>>().expect("cannot format datetime");
+            Some(commit_ts - dt.timestamp())
         },
-        Err(e) => {
-            println!("Token not found!");
-            Err(Box::new(e))
-        }
+        None => None
     }
 }
 
@@ -75,7 +66,7 @@ fn extract_pr_from_commit_message(commit_message: &str) -> Option<&str> {
     }
 }
 
-fn get_commit_log(repo: Repository, from: &str, to: &str, github_repo: &str) -> Result<String, Error> {
+fn get_commit_log(access_token: &str, repo: Repository, from: &str, to: &str, github_repo: &str) -> Result<String, Error> {
     let f = repo.revparse_single(from)?;
     let t = repo.revparse_single(to)?;
     let mut revwalk = repo.revwalk()?;
@@ -87,7 +78,7 @@ fn get_commit_log(repo: Repository, from: &str, to: &str, github_repo: &str) -> 
 
     let commit_list: Vec<String> = revwalk.map(|c| {
         let commit = repo.find_commit(c.unwrap()).unwrap();
-        commit_to_formatted_output(commit, github_repo).unwrap()
+        commit_to_formatted_output(commit, github_repo, access_token).unwrap()
     }).collect();
 
     let output = commit_list.join("\n");
@@ -103,15 +94,23 @@ Usage: branch-time <git_repo_path> <github_repo> <from_tag> <to_tag>
     let args = Docopt::new(USAGE)
         .and_then(|d| d.parse())
         .unwrap_or_else(|e| e.exit());
-    let processed_commits = get_commit_log(
-        Repository::open(
-            args.get_str("<git_repo_path>")).expect("failed to open repo"),
-        args.get_str("<from_tag>"),
-        args.get_str("<to_tag>"),
-    args.get_str("<github_repo>")).expect("unable to get commit log");
 
-    let output_file = format!("/tmp/branch-times-{}-{}.csv", args.get_str("<from_tag>").replace("/", "-"), args.get_str("<to_tag>").replace("/", "-"));
-    fs::write(&output_file, processed_commits).expect(&format!("couldn't write to file: {}", &output_file));
+    match env::var("GITHUB_STATS_TOKEN") {
+        Ok(access_token) => {
+            let processed_commits = get_commit_log(&access_token,
+                Repository::open(
+                    args.get_str("<git_repo_path>")).expect("failed to open repo"),
+                args.get_str("<from_tag>"),
+                args.get_str("<to_tag>"),
+                args.get_str("<github_repo>")).expect("unable to get commit log");
+
+            let output_file = format!("/tmp/branch-times-{}-{}.csv", args.get_str("<from_tag>").replace("/", "-"), args.get_str("<to_tag>").replace("/", "-"));
+            fs::write(&output_file, format!("commit_sha,commit_ts,pull_request,branch_time_seconds,author,message\n{}",processed_commits)).expect(&format!("couldn't write to file: {}", &output_file));
+        },
+        Err(e) => {
+            panic!("Token not found! {}", e);
+        }
+    }
 }
 
 
@@ -121,26 +120,29 @@ mod tests {
 
     #[test]
     fn test_get_commit_log() {
+        let access_token = env::var("GITHUB_STATS_TOKEN").expect("Token not found");
         let repo = Repository::open("/Users/kevj/projects/voyager").expect("cannot open git repo");
-        let r = get_commit_log(repo, "origin/release/2.167.x", "origin/release/2.168.x", "THG-Voyager/voyager");
+        let r = get_commit_log(&access_token, repo, "origin/release/2.167.x", "origin/release/2.168.x", "THG-Voyager/voyager");
         assert!(r.is_ok());
     }
 
     #[test]
     fn test_commit_to_formatted_output() {
+        let access_token = env::var("GITHUB_STATS_TOKEN").expect("Token not found");
         let repo = Repository::open("/Users/kevj/projects/voyager").expect("cannot open git repo");
         let commit_id = Oid::from_str("77728b3066ce7b179acdfac776512f570fffdaae").unwrap();
         let commit = repo.find_commit(commit_id).unwrap();
-        let r = commit_to_formatted_output(commit, "THG-Voyager/voyager");
+        let r = commit_to_formatted_output(commit, "THG-Voyager/voyager", &access_token);
         assert!(r.is_ok());
-        assert_eq!("77728b3066ce7b179acdfac776512f570fffdaae,1522335500,4132,VGR-8087 - Adding tests for verifying required products service is decremented (#4729)", r.unwrap())
+        assert_eq!("77728b3066ce7b179acdfac776512f570fffdaae,1522335500,4729,4132,zohaib.m.khan96@gmail.com,VGR-8087 - Adding tests for verifying required products service is decremented (#4729)", r.unwrap())
     }
 
     #[test]
     fn test_fetch_github_info_for_commit() {
+        let access_token = env::var("GITHUB_STATS_TOKEN").expect("Token not found");
         let pr_number = "4729";
-        let r = fetch_github_info_for_commit(1522335500, pr_number, "THG-Voyager/voyager");
-        assert!(r.is_ok());
+        let r = fetch_github_info_for_commit(1522335500, pr_number, "THG-Voyager/voyager", &access_token);
+        assert!(r.is_some());
     }
 
     #[test]
